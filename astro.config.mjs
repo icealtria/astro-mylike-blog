a24698ef7f8040f042cbf4b191da4f934e32cfe7
Rin <icealtria@gmail.com>
2023-08-24 16:45:02 +0800

    css adjustments && fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,14 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	output: 'static',
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					assetFileNames: 'assets/[name][extname]',
+				}
+			}
+		}
+	}
 });
