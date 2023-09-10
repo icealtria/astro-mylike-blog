5311f879bf428b6bff42d3e53203b2c99321136a
icealtria <icealtria@gmail.com>
2023-09-10 22:40:50 +0800

    WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,25 @@
 const { withMaterialColors } = require('tailwind-material-colors');
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMaterialColors({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-
+		extend: {
+			gridTemplateColumns: {
+				'fill': 'repeat(auto-fit, minmax(32ch, 1fr));',
+			  },
+			  borderRadius: {
+				'4xl': '2rem',
+			},
+		},
 	},
-	plugins: [],
-}
-
-
-module.exports = withMaterialColors({
+	plugins: [require('@tailwindcss/typography'),],
 	// Here, your tailwind config.
 	// (Do not specify theme.colors or theme.extend.colors as they will be overwritten).
 }, {
 	// Here, your base colors as HEX values
 	// primary is required
 	// secondary and/or tertiary are optional, if not set they will be derived from the primary color
-	primary: '#ff0000',
-	secondary: '#ffff00',
-	tertiary: '#0000ff',
-	// extra named colors may also be included
-	green: '#00ff00'
-	blue: '#0000ff'
+	// primary: '#405e0d',
+	primary: '#586500',
 });
